ec9ca6fae3a1c058829fc74d10194cb39bc2e34e
khhuang <khhuang@wisdomgarden.com>
2021-02-24 15:40:18 +0800

    fix ci/notify_tc_pkg_to_ms_teams.sh script

--- ci/notify_tc_pkg_to_ms_teams.sh
+++ ci/notify_tc_pkg_to_ms_teams.sh
@@ -6,9 +6,14 @@ MS_TEAMS_WEBHOOK=${MS_TEAMS_WEBHOOK}
 ENABLE_MS_TEAMS_NOTIFY=${ENABLE_MS_TEAMS_NOTIFY:false}
 PACKAGE_VERSION=${PACKAGE_VERSION}
 
+echo "MS_TEAMS_WEBHOOK: $MS_TEAMS_WEBHOOK"
+echo "ENABLE_MS_TEAMS_NOTIFY: $ENABLE_MS_TEAMS_NOTIFY"
+echo "PACKAGE_VERSION: $PACKAGE_VERSION"
 
-if [ "$ENABLE_MS_TEAMS_NOTIFY" == "true" ]; then
-  curl $MS_TEAMS_WEBHOOK -X POST \
-  -h 'Content-Type: application/json' \
-  -d '{"title": "LMS New Package:", "text": "$PACKAGE_VERSION"}'
+
+if [ "$ENABLE_MS_TEAMS_NOTIFY" == "true" &&  ]; then
+  curl $MS_TEAMS_WEBHOOK \
+  --header 'Content-Type: application/json' \
+  --data "{\"title\": \"LMS New Package:\", \"text\": \"${PACKAGE_VERSION}\"}" \
+  -X POST
 fi
