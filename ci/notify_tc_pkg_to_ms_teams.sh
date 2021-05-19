1f24a4023c742c8a24d1e0387c7c0e930fa991dd
khhuang <khhuang@wisdomgarden.com>
2021-05-19 11:17:56 +0800

    Update notify_tc_pkg_to_ms_teams.sh

--- ci/notify_tc_pkg_to_ms_teams.sh
+++ ci/notify_tc_pkg_to_ms_teams.sh
@@ -6,6 +6,9 @@ MS_TEAMS_WEBHOOK=${MS_TEAMS_WEBHOOK}
 ENABLE_MS_TEAMS_NOTIFY=${ENABLE_MS_TEAMS_NOTIFY:false}
 PACKAGE_VERSION=${PACKAGE_VERSION}
 
+# Version Tracker URL
+VT_URL=${VT_URL}
+
 echo "MS_TEAMS_WEBHOOK: $MS_TEAMS_WEBHOOK"
 echo "ENABLE_MS_TEAMS_NOTIFY: $ENABLE_MS_TEAMS_NOTIFY"
 echo "PACKAGE_VERSION: $PACKAGE_VERSION"
@@ -14,6 +17,9 @@ echo "PACKAGE_VERSION: $PACKAGE_VERSION"
 if [ "$ENABLE_MS_TEAMS_NOTIFY" == "true" &&  ]; then
   curl $MS_TEAMS_WEBHOOK \
   --header 'Content-Type: application/json' \
-  --data "{\"title\": \"LMS New Package:\", \"text\": \"${PACKAGE_VERSION}\"}" \
+  --data "{
+      \"title\": \"LMS New Package:\", 
+      \"text\": \"Version: ${PACKAGE_VERSION}<br/>Please check & update [Version Tracker](${VT_URL})\"
+  }" \
   -X POST
 fi
